0aa341302c04e222ed75d6f4eb1a2393a1e8e83e
Michael Revell <mikearevell@gmail.com>
2013-07-06 14:56:52 -0700

    moarz

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -6,10 +6,10 @@ def get_keychain_pass(account=None, server=None):
         'command': 'find-internet-password',
         'account': account,
         'server': server,
-        'keychain': '/Users/`whoami`/Library/Keychains/login.keychain',
+        'keychain': '/Users/Mars/Library/Keychains/login.keychain',
     }
-    command = "security find-generic-password -w -s %(server)s -a %(account)s"
-    #command = "sudo -u `whoami` %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    #command = "security find-generic-password -w -s %(server)s -a %(account)s"
+    command = "sudo -u Mars %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
     output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
     outtext = [l for l in output.splitlines()
                if l.startswith('password: ')][0]
